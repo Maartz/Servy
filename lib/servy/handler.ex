6a0b373389c5a10b56a2fa0c58a51d20df6770f3
maartz <maartz@protonmail.com>
2020-05-06 15:52:16 +0200

    Add rewrite url def + unnecessary emojify function + DELETE HTTP def

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,8 +3,11 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> emojify
+    |> track
     |> format_response
   end
 
@@ -19,6 +22,24 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
+  # TODO: Rewrite with ?id should leverage Regex
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: "/bears?id=" <> id } = conv) do
+    %{conv | path: "/bears/" <> id}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} does not exists"
+    conv
+  end
+
+  def track(conv), do: conv
 
   def route(%{path: "/wildthings", method: "GET"} = conv) do
     %{conv | resp_body: "Bears, Lions, Tigers ", status: 200}
@@ -30,6 +51,10 @@ defmodule Servy.Handler do
 
   def route(%{path: "/bears/" <> id, method: "GET"} = conv) do
     %{conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(%{path: "/bears/" <> id, method: "DELETE"} = conv) do
+    %{conv | resp_body: "You're not allowed to delete 'Bear #{id}'", status: 403}
   end
 
   def route(conv) do
@@ -47,8 +72,7 @@ defmodule Servy.Handler do
   end
 
   defp status_reason(code) do
-    %{
-      200 => "OK",
+    %{ 200 => "OK",
       201 => "Created",
       401 => "Unauthorized",
       403 => "Forbidden",
@@ -56,6 +80,14 @@ defmodule Servy.Handler do
       500 => "Internal Server Error"
     }[code]
   end
+
+  defp emojify(%{status: 200} = conv) do
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+    %{conv | resp_body: body}
+  end
+
+  defp emojify(conv), do: conv
 
 end
 
@@ -106,5 +138,48 @@ Accept: */*
 response = Servy.Handler.handle(request)
 IO.puts response
 
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /fourohfour HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
 
