37afec5ca2ed469a45d02026723d864ff4896cd5
maartz <maartz@protonmail.com>
2020-05-07 22:42:05 +0200

    Create a Conv struct and update all conv to be Conv structs

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,30 +1,31 @@
 defmodule Servy.Plugins do
 
+  alias Servy.Conv
   require Logger
   # TODO: Rewrite with ?id should leverage Regex
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id } = conv) do
+  def rewrite_path(%Conv{path: "/bears?id=" <> id } = conv) do
     %{conv | path: "/bears/" <> id}
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%Conv{} = conv), do: conv
 
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     IO.puts "Error: #{path} does not exists"
     Logger.error conv.path
     conv
   end
 
-  def track(%{method: "DELETE"} = conv) do
+  def track(%Conv{method: "DELETE"} = conv) do
     Logger.warn conv.path
     conv
   end
 
-  def track(conv) do
+  def track(%Conv{} = conv) do
     Logger.info conv.path
     conv
   end
