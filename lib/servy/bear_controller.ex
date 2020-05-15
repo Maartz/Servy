75839b643f257ce17ecbc72a2bfa3faf0bf2235e
maartz <maartz@protonmail.com>
2020-05-15 14:26:52 +0200

    Move render in its own module

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,30 +1,19 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-
-  @templates_path Path.expand("../../templates", __DIR__)
-
-  defp render(conv, template, bindings \\ []) do
-
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | resp_body: content, status: 200}
-  end
+  alias Servy.View
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Bear.alphabetical_sort()
 
-    render(conv, "index.eex", bears: bears)
+    View.render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    render(conv, "show.eex", bear: bear)
+      View.render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, params) do
